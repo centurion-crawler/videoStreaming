23674a2919f6bbc7c12d7425184df4ef65c2fed7
centurion-crawler <centurion_crawler@outlook.com>
2020-11-03 23:02:10 +0800

    v 1.0

---
+++ src/com/actions/Test.java
@@ -0,0 +1,18 @@
+package com.actions;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+		System.out.println((76/10));
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Test();
+	}
+
+}
